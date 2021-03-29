92bf2d3d54c93bd14c3bbccc5ba5087f65a37d87
wumingover <2875389727@qq.com>
2021-03-29 09:06:26 +0800

    Initial commit

---
+++ src/main/java/com/lianshui/chenshi/ChenshiApplication.java
@@ -0,0 +1,13 @@
+package com.lianshui.chenshi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChenshiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ChenshiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/lianshui/chenshi/ChenshiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lianshui.chenshi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChenshiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
